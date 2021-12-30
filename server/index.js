6c56c4b6159f823bbf9109f0da570c1ca2ac1591
danipatko <p8.pada@gmail.com>
2021-12-30 21:44:48 +0100

    fix typo

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import ewelink from 'ewelink-api';
 const app = express();
 // my socket's ID 1000cc4a9e
 
-const connection =  new ewelink({ email: process.env.EVELINK_EMAIL || '', password: process.env.EVELINK_PASSWORD || '' });
+const connection =  new ewelink({ email: process.env.EWELINK_EMAIL || '', password: process.env.EWELINK_PASSWORD || '' });
 
 // console.log(await connection.getDevice('1000cc4a9e'))
 
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: false }));
  * Switch socket on
  */
 app.post('/:id/on', async (req, res, next)  => {
-    console.log(`Received request on for ${req.params.id}`);
+    // console.log(`Received request on for ${req.params.id}`); // DEBUG
     const device = await connection.getDevice(req.params.id);
     
     if(device && device.params && device.params.switch != 'on')
@@ -28,7 +28,7 @@ app.post('/:id/on', async (req, res, next)  => {
  * Switch socket off
  */
 app.post('/:id/off', async (req, res, next)  => {
-    console.log(`Received request off for ${req.params.id}`);
+    // console.log(`Received request off for ${req.params.id}`); // DEBUG
     const device = await connection.getDevice(req.params.id);
     
     if(device && device.params && device.params.switch == 'on')
